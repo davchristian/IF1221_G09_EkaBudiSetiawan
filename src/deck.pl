5a9a2b62265d0da532a56ca0ae95a59fa762dd06
david <David Christian>
2026-05-11 17:48:20 +0700

    feat: fakta deck

---
+++ src/deck.pl
@@ -0,0 +1,71 @@
+kartu(angka, kuning, 0).
+kartu(angka, kuning, 1).
+kartu(angka, kuning, 2).
+kartu(angka, kuning, 3).
+kartu(angka, kuning, 4).
+kartu(angka, kuning, 5).
+kartu(angka, kuning, 6).
+kartu(angka, kuning, 7).
+kartu(angka, kuning, 8).
+kartu(angka, kuning, 9).
+
+kartu(angka, merah, 0).
+kartu(angka, merah, 1).
+kartu(angka, merah, 2).
+kartu(angka, merah, 3).
+kartu(angka, merah, 4).
+kartu(angka, merah, 5).
+kartu(angka, merah, 6).
+kartu(angka, merah, 7).
+kartu(angka, merah, 8).
+kartu(angka, merah, 9).
+
+kartu(angka, biru, 0).
+kartu(angka, biru, 1).
+kartu(angka, biru, 2).
+kartu(angka, biru, 3).
+kartu(angka, biru, 4).
+kartu(angka, biru, 5).
+kartu(angka, biru, 6).
+kartu(angka, biru, 7).
+kartu(angka, biru, 8).
+kartu(angka, biru, 9).
+
+kartu(angka, hijau, 0).
+kartu(angka, hijau, 1).
+kartu(angka, hijau, 2).
+kartu(angka, hijau, 3).
+kartu(angka, hijau, 4).
+kartu(angka, hijau, 5).
+kartu(angka, hijau, 6).
+kartu(angka, hijau, 7).
+kartu(angka, hijau, 8).
+kartu(angka, hijau, 9).
+
+kartu(reverse, kuning, reverse).
+kartu(reverse, merah, reverse).
+kartu(reverse, biru, reverse).
+kartu(reverse, hijau, reverse).
+
+kartu(skip, kuning, skip).
+kartu(skip, merah, skip).
+kartu(skip, biru, skip).
+kartu(skip, hijau, skip).
+
+kartu(drawTwo, kuning, drawTwo).
+kartu(drawTwo, merah, drawTwo).
+kartu(drawTwo, biru, drawTwo).
+kartu(drawTwo, hijau, drawTwo).
+
+kartu(wild, hitam, wild).
+kartu(wild, hitam, wild).
+kartu(wild, hitam, wild).
+kartu(wild, hitam, wild).
+
+kartu(wildFour, hitam, wildFour).
+kartu(wildFour, hitam, wildFour).
+kartu(wildFour, hitam, wildFour).
+kartu(wildFour, hitam, wildFour).
+
+
+

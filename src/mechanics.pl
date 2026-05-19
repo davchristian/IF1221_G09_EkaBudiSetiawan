511f16e9c2b820ad9a4a5f585aa834f6e8b332cd
Mulky Siraj Firizqi <mulky_sf@Mulkys-MacBook-Air.local>
2026-05-19 09:05:39 +0700

    feat: integrate milestone 2 logic, setup reset, and clean up duplicate commands

--- src/mechanics.pl
+++ src/mechanics.pl
@@ -1,127 +1,149 @@
-% Deklarasi dinamis
-:- dynamic(kartu_pemain/2).
-:- dynamic(discard_top/1).
 :- dynamic(status_UNI/1).
+:- dynamic(warna_aktif/1).
+:- dynamic(menunggu_respons_draw_two/0).
+:- dynamic(menunggu_respons_draw_four/2).
 
-% 1. mainkanKartu_mechanics(NomorUrut)
-mainkanKartu_mechanics(_) :-
-    \+ gameMulai, !,
-    write('Game belum dimulai! Gunakan startGame.').
+mainkanKartu(_) :-
+    \+ gameMulai, !, write('Game belum dimulai! Gunakan startGame.'), nl.
 
-mainkanKartu_mechanics(NomorUrut) :-
+mainkanKartu(_) :-
+    menunggu_respons_draw_four(_, _), !,
+    write('Anda terkena efek Wild Draw Four! Anda hanya bisa menggunakan perintah "tantang." atau "ambilKartu."'), nl.
+
+mainkanKartu(_) :-
+    menunggu_respons_draw_two, !,
+    write('Anda terkena efek Draw Two! Anda harus menggunakan perintah "ambilKartu."'), nl.
+
+mainkanKartu(Nomor) :-
     giliran(Pemain),
-    kartu_pemain(Pemain, Hand),
-
-    % Validasi apakah NomorUrut ada di dalam range list kartu di tangan
-    length(Hand, Len),
-    (NomorUrut < 1 ; NomorUrut > Len), !,
-    write('Nomor urut kartu tidak valid! Coba lihatKartu lagi.').
-
-mainkanKartu_mechanics(NomorUrut) :-
-    giliran(Pemain),
-    kartu_pemain(Pemain, Hand),
-
-    % Ambil kartu dari tangan berdasarkan NomorUrut
-    nth1(NomorUrut, Hand, KartuPilihan),
-    KartuPilihan = kartu(Tipe, Warna, Nilai),
-
-    % Ambil info kartu teratas di meja
-    discard_top(KartuTop),
-    KartuTop = kartu(_, WarnaTop, NilaiTop),
-
-    % Validasi: Warna sama, nilai/tipe sama, atau kartu wild (warna hitam)
-    (Warna == WarnaTop ; Nilai == NilaiTop ; Warna == hitam), !,
-
-    % Eksekusi: Hapus kartu dari tangan
-    hapus_elemen(NomorUrut, Hand, NewHand),
-    retract(kartu_pemain(Pemain, _)),
-    asserta(kartu_pemain(Pemain, NewHand)),
-
-    % Eksekusi: Update discard pile di meja
-    retract(discard_top(_)),
-    asserta(discard_top(KartuPilihan)),
-
-    format('~w memainkan kartu: ~w-~w.~n', [Pemain, Warna, Nilai]),
-
-    % Pindah giliran ke pemain selanjutnya (belum efek skip/reverse)
-    nextTurn.
-
-% Jika kondisi di atas gagal (kartu tidak cocok)
-mainkanKartu_mechanics(_) :-
-    write('Kartu tidak valid! Silakan masukkan pilihan kartu kembali.').
-
-% 2. ambilKartu_mechanics
-ambilKartu_mechanics :-
-    \+ gameMulai, !,
-    write('Game belum dimulai! Gunakan startGame.').
-
-ambilKartu_mechanics :-
-    giliran(Pemain),
-    kartu_pemain(Pemain, Hand),
-
-    % Ambil satu kartu acak (memanggil helper)
-    get_random_card(KartuBaru),
-
-    % Tambahkan ke tangan
-    append(Hand, [KartuBaru], NewHand),
-    retract(kartu_pemain(Pemain, _)),
-    asserta(kartu_pemain(Pemain, NewHand)),
-
-    format('~w mendapatkan kartu baru.~n', [Pemain]),
-
-    % Pindah giliran
-    nextTurn.
-
-% HELPER FUNCTION (membagi 7 kartu di awal + set kartu pertama di discard pile)
-
-% Helper: Pindah giliran
-nextTurn :-
-    giliran(PemainSekarang),
-    daftarPemain(Urutan),
-
-    % Cari siapa pemain selanjutnya di dalam list
-    next_player(PemainSekarang, Urutan, PemainSelanjutnya),
-
-    % Update giliran
-    retract(giliran(_)),
-    asserta(giliran(PemainSelanjutnya)),
-    nl, format('Giliran ~w.~n', [PemainSelanjutnya]).
-
-% Mencari pemain selanjutnya (circular list)
-next_player(X, List, Next) :-
-    append(_, [X, Next | _], List), !.
-next_player(X, List, Next) :-
-    last(List, X),
-    List = [Next | _], !.
-
-% Helper: Hapus elemen berdasarkan Index
-hapus_elemen(1, [_|T], T) :- !.
-hapus_elemen(N, [H|T], [H|R]) :-
-    N1 is N - 1,
-    hapus_elemen(N1, T, R).
-
-% Helper (sementara untuk test code): Ambil kartu acak dari deck.pl
-get_random_card(kartu(Tipe, Warna, Nilai)) :-
-    findall(kartu(T, W, N), kartu(T, W, N), SemuaKartu),
-    random_member(kartu(Tipe, Warna, Nilai), SemuaKartu).
-
-
-
-% Helper untuk menarik N kartu penalti secara aman dari deck.pl
-tarik_kartu_penalti(N, Pemain) :-
-    deck(DeckSekarang),
-    playerCard(Pemain, TanganSekarang),
-    ( ambilNKartu(N, DeckSekarang, KartuPenalti, DeckBaru) ->
-        append(TanganSekarang, KartuPenalti, TanganBaru),
-        retract(playerCard(Pemain, TanganSekarang)),
-        assertz(playerCard(Pemain, TanganBaru)),
-        retract(deck(DeckSekarang)),
-        assertz(deck(DeckBaru))
-    ;
-        write('[Sistem] Deck tidak cukup untuk penalti!'), nl
+    playerCard(Pemain, Hand),
+    ( ambilKartuN(Nomor, Hand, KartuPilihan) ->
+        activeCard(KartuTop),
+        
+        (warna_aktif(WA) -> WarnaAktif = WA ; KartuTop = kartu(WarnaAktif, _)),
+        ( proses_kartu(Pemain, Nomor, KartuPilihan, KartuTop, WarnaAktif, Hand) ->
+            playerCard(Pemain, HandBaru),
+            ( HandBaru == [] -> endGame ; true )
+        ; write('Kartu tidak valid untuk dimainkan atau dilarang menumpuk kartu aksi yang sama!'), nl
+        )
+    ; write('Nomor kartu salah.'), nl
     ).
 
-% 3. uni(NomorUrutKartuDiTangan)
+proses_kartu(Pemain, Nomor, kartu(hitam, wildFour), kartu(_, JenisTop), WarnaAktif, Hand) :-
+    JenisTop \= wildFour, !,
+    ( punya_kartu_cocok(Hand, WarnaAktif) -> Legalitas = ilegal ; Legalitas = legal ),
+    buang_dan_update(Pemain, Nomor, kartu(hitam, wildFour)),
+    minta_warna_baru(WarnaBaru),
+    retractall(warna_aktif(_)), asserta(warna_aktif(WarnaBaru)),
+    asserta(menunggu_respons_draw_four(Pemain, Legalitas)),
+    format('~w memainkan Wild Draw Four! Warna menjadi ~w.~n', [Pemain, WarnaBaru]),
+    pindahGiliran(Pemain).
+
+proses_kartu(Pemain, Nomor, kartu(hitam, wild), kartu(_, JenisTop), _, _) :-
+    JenisTop \= wild, !,
+    buang_dan_update(Pemain, Nomor, kartu(hitam, wild)),
+    minta_warna_baru(WarnaBaru),
+    retractall(warna_aktif(_)), asserta(warna_aktif(WarnaBaru)),
+    format('~w memainkan Wild! Warna menjadi ~w.~n', [Pemain, WarnaBaru]),
+    pindahGiliran(Pemain).
+
+proses_kartu(Pemain, Nomor, kartu(Warna, drawTwo), kartu(_, JenisTop), WarnaAktif, _) :-
+    JenisTop \= drawTwo,
+    Warna == WarnaAktif, !,
+    buang_dan_update(Pemain, Nomor, kartu(Warna, drawTwo)),
+    retractall(warna_aktif(_)), asserta(warna_aktif(Warna)),
+    asserta(menunggu_respons_draw_two),
+    format('~w memainkan Draw Two! Pemain selanjutnya akan terkena denda.~n', [Pemain]),
+    pindahGiliran(Pemain).
+
+proses_kartu(Pemain, Nomor, kartu(Warna, Jenis), kartu(_, JenisTop), WarnaAktif, _) :-
+    Jenis \= wild, Jenis \= wildFour, Jenis \= drawTwo,
+    (Warna == WarnaAktif ; Jenis == JenisTop), !,
+    buang_dan_update(Pemain, Nomor, kartu(Warna, Jenis)),
+    retractall(warna_aktif(_)), asserta(warna_aktif(Warna)),
+    format('~w memainkan kartu: ~w-~w.~n', [Pemain, Warna, Jenis]),
+    pindahGiliran(Pemain). 
+
+tantang :-
+    \+ gameMulai, !, write('Game belum dimulai!'), nl.
+
+tantang :-
+    giliran(Penantang),
+    menunggu_respons_draw_four(Pelempar, Legalitas), !,
+    write('Tantangan dilakukan!'), nl,
+    format('Memeriksa kartu ~w...~n', [Pelempar]),
+    ( Legalitas == ilegal ->
+        write('Tantangan BERHASIL!'), nl,
+        format('~w terbukti memiliki kartu yang cocok dan mendapatkan 4 kartu penalti.~n', [Pelempar]),
+        tarik_kartu_penalti(4, Pelempar)
+    ;
+        write('Tantangan GAGAL.'), nl,
+        format('~w mendapatkan 6 kartu acak sebagai hukuman!~n', [Penantang]),
+        tarik_kartu_penalti(6, Penantang)
+    ),
+    retractall(menunggu_respons_draw_four(_,_)),
+    pindahGiliran(Penantang).
+
+tantang :-
+    write('Tidak ada penggunaan Wild Draw Four yang bisa ditantang saat ini.'), nl.
+
+ambilKartu :-
+    \+ gameMulai, !, write('Game belum dimulai!'), nl.
+
+ambilKartu :-
+    giliran(Pemain),
+    ( menunggu_respons_draw_two ->
+        write('Mengambil 2 kartu karena efek Draw Two...'), nl,
+        tarik_kartu_penalti(2, Pemain),
+        retractall(menunggu_respons_draw_two)
+    ; menunggu_respons_draw_four(_, _) ->
+        write('Anda memilih tidak menantang.'), nl,
+        write('Mengambil 4 kartu karena efek Wild Draw Four...'), nl,
+        tarik_kartu_penalti(4, Pemain),
+        retractall(menunggu_respons_draw_four(_,_))
+    ;
+        tarik_kartu_penalti(1, Pemain),
+        format('~w mendapatkan 1 kartu baru.~n', [Pemain])
+    ),
+    pindahGiliran(Pemain).
+
+% Helper Functions
+buang_dan_update(Pemain, Nomor, KartuPilihan) :-
+    playerCard(Pemain, Hand),
+    deleteKartu(Nomor, Hand, NewHand),
+    retract(playerCard(Pemain, _)), assertz(playerCard(Pemain, NewHand)),
+    retract(activeCard(_)), assertz(activeCard(KartuPilihan)).
+
+minta_warna_baru(WarnaBaru) :-
+    write('Pilih warna (merah/kuning/hijau/biru): '),
+    read(Input),
+    ( member(Input, [merah, kuning, hijau, biru]) -> WarnaBaru = Input
+    ; write('Warna tidak valid! Coba lagi.'), nl, minta_warna_baru(WarnaBaru) ).
+
+punya_kartu_cocok(Hand, WarnaAktif) :- member(kartu(WarnaAktif, _), Hand), !.
+punya_kartu_cocok(Hand, _) :- activeCard(kartu(_, JenisTop)), member(kartu(_, JenisTop), Hand), !.
+
+tarik_kartu_penalti(0, _) :- !.
+tarik_kartu_penalti(N, Pemain) :-
+    N > 0,
+    deck(DeckSekarang),
+    playerCard(Pemain, TanganSekarang),
+    ( ambilNKartu(1, DeckSekarang, [KartuBaru], DeckBaru) ->
+        append(TanganSekarang, [KartuBaru], TanganBaru),
+        retract(playerCard(Pemain, _)), assertz(playerCard(Pemain, TanganBaru)),
+        retract(deck(_)), assertz(deck(DeckBaru)),
+        N1 is N - 1,
+        tarik_kartu_penalti(N1, Pemain)
+    ;
+        write('[Sistem] Deck habis! Tidak bisa menarik kartu lagi.'), nl
+    ).
+
+pindahGiliran(Pemain) :-
+    nextPlayer(PemainBerikutnya), 
+    retract(giliran(Pemain)),
+    assertz(giliran(PemainBerikutnya)),
+    format('Giliran ~w.~n', [PemainBerikutnya]).
+
 uni(NomorUrut) :-
     giliran(Pemain),
     playerCard(Pemain, Tangan),
@@ -144,27 +166,20 @@ uni(NomorUrut) :-
             assertz(activeCard(KartuPilihan)),
             
             assertz(status_UNI(Pemain)),
-            
-            nextPlayer(PemainBerikutnya),
-            retract(giliran(Pemain)),
-            assertz(giliran(PemainBerikutnya)),
-            format('Giliran ~w.~n', [PemainBerikutnya])
+            pindahGiliran(Pemain)
             
         ; 
             format('Kartu tidak valid! ~w mendapatkan 1 kartu penalti.~n', [Pemain]),
             tarik_kartu_penalti(1, Pemain),
-            nextPlayer(PemainBerikutnya), retract(giliran(_)), assertz(giliran(PemainBerikutnya)),
-            format('Giliran ~w.~n', [PemainBerikutnya])
+            pindahGiliran(Pemain)
         )
     ; 
         format('Perintah tidak valid! ~w tidak memenuhi syarat UNI.~n', [Pemain]),
         format('~w mendapatkan 1 kartu penalti.~n', [Pemain]),
         tarik_kartu_penalti(1, Pemain),
-        nextPlayer(PemainBerikutnya), retract(giliran(_)), assertz(giliran(PemainBerikutnya)),
-        format('Giliran ~w.~n', [PemainBerikutnya])
+        pindahGiliran(Pemain)
     ).
 
-% 4. tangkap(NamaPemain)
 tangkap(Target) :-
     giliran(PemainAktif),
     ( PemainAktif == Target -> 
@@ -177,9 +192,7 @@ tangkap(Target) :-
             format('Tantangan berhasil! ~w tertangkap tidak menyerukan UNI.~n', [Target]),
             format('~w mendapatkan 2 kartu penalti.~n', [Target]),
             tarik_kartu_penalti(2, Target),
-            
-            nextPlayer(Next), retract(giliran(_)), assertz(giliran(Next)),
-            format('Giliran ~w.~n', [Next])
+            pindahGiliran(Target)
         ;
             format('Tantangan gagal. ~w aman atau tidak melanggar aturan.~n', [Target]),
             format('~w mendapatkan 1 kartu penalti secara acak.~n', [PemainAktif]),
@@ -187,7 +200,6 @@ tangkap(Target) :-
         )
     ).
 
-% 5. Deteksi Endgame
 poin_kartu(kartu(_, J), Poin) :- integer(J), Poin = J, !.
 poin_kartu(kartu(_, skip), 10) :- !.
 poin_kartu(kartu(_, reverse), 10) :- !.
--- src/setup.pl
+++ src/setup.pl
@@ -43,8 +43,11 @@ resetGame :-
     retractall(deck(_)),
     retractall(activeCard(_)),
     retractall(kartu_pemain(_, _)),
-    retractall(discard_top(_)).
-    retractall(status_UNI(_)).
+    retractall(discard_top(_)),
+    retractall(status_UNI(_)),
+    retractall(warna_aktif(_)),
+    retractall(menunggu_respons_draw_two),
+    retractall(menunggu_respons_draw_four(_, _)).
 
 tampilUrutan([X]) :-
     write(X).

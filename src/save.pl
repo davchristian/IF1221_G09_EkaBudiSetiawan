6de3b92a26f8517bb76ffb9bc09055757ec7914d
david <David Christian>
2026-05-30 15:11:56 +0700

    feat: add save.pl:

--- src/save.pl
+++ src/save.pl
@@ -1,5 +1,6 @@
 saveGame :-
-    \+ gameMulai, !, write('Game belum dimulai!'), nl.
+    \+ gameMulai, !,
+    write('Game belum dimulai!'), nl.
 saveGame :-
     menunggu_respons_draw_four(_, _), !,
     write('Pilih "tantang." atau "ambilKartu." sebelum save.'), nl.
@@ -16,33 +17,52 @@ saveGame :-
     format('Status permainan berhasil disimpan ke ~w.~n', [NamaFileTxt]).
 
 gameState(Data) :-
+    % urutan
     daftarPemain(DaftarPemain),
-    format(Data, 'urutan_pemain(~w).~n', [DaftarPemain]),
+    write(Data, 'urutan_pemain:'),
+    write_quoted_list(Data, DaftarPemain),
+    write(Data, '.'), nl(Data),
+    % giliran
     giliran(Pemain),
-    format(Data, 'giliran(~w).~n', [Pemain]),
+    write(Data, 'giliran:'),
+    writeq(Data, Pemain),
+    write(Data, '.'), nl(Data),
+    % discard top
     activeCard(KartuTop),
     cardString(KartuTop, StringKartu),
     format(Data, 'discard_top:~w.~n', [StringKartu]),
-    ( warna_aktif(Warna) -> true 
-    ; KartuTop = kartu(Warna, _) ),
+    % warna aktif
+    ( warna_aktif(Warna) -> true
+    ; KartuTop = kartu(Warna, _)),
     format(Data, 'warna_aktif:~w.~n', [Warna]),
+    % arah game
     arahPermainan(Arah),
     arahGameString(Arah, StringArah),
     format(Data, 'arah_permainan:~w.~n', [StringArah]),
+    % uni
     findall(P, status_UNI(P), ListUNI),
-    format(Data, 'status_UNI(~w).~n', [ListUNI]),
+    write(Data, 'status_UNI:'),
+    write_quoted_list(Data, ListUNI),
+    write(Data, '.'), nl(Data),
+    % kartu player
     saveCard(Data, DaftarPemain).
 
 saveCard(_, []).
 saveCard(Data, [H|Sisa]) :-
-    kartu_pemain(H, ListKartu), 
+    playerCard(H, ListKartu),
     listCardString(ListKartu, StringCards),
-    format(Data, 'kartu(\'~w\'):[~w].~n', [H, StringCards]),
+    write(Data, 'kartu('),
+    writeq(Data, H),
+    write(Data, '):['),
+    write(Data, StringCards),
+    write(Data, '].'),
+    nl(Data),
     saveCard(Data, Sisa).
 
 cardString(kartu(Warna, Jenis), String) :-
-    ( integer(Jenis) -> number_atom(Jenis, JenisAtom) 
-    ; JenisAtom = Jenis ),
+    ( integer(Jenis) -> number_codes(Jenis, Codes),
+    atom_codes(JenisAtom, Codes)
+    ; JenisAtom = Jenis),
     atom_concat(Warna, '-', Tmp),
     atom_concat(Tmp, JenisAtom, String).
 
@@ -54,6 +74,19 @@ listCardString([K|Sisa], String) :-
     listCardString(Sisa, StringSisa),
     atom_concat(StringK, ',', Tmp),
     atom_concat(Tmp, StringSisa, String).
+
+writeListFormat(Data, List) :-
+    write(Data, '['),
+    writeList(Data, List),
+    write(Data, ']').
+
+writeList(_, []) :- !.
+writeList(Data, [H]) :-
+    writeq(Data, H), !.
+writeList(Data, [H|T]) :-
+    writeq(Data, H),
+    write(Data, ','),
+    writeList(Data, T).
 
 arahGameString(clockwise, kanan).
 arahGameString(counterclockwise, kiri).
